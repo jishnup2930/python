0ecfea2d5c9b6790a8055931d3752e9f114a1a7d
jishnu <givemeakiss111@gmail.com>
2023-10-28 22:37:26 +0530

    new

---
+++ ex8.py
@@ -0,0 +1,10 @@
+formatter =' {} {} {} {}'
+print(formatter.format(1,2,3,4))
+print(formatter.format('one','two','three','four'))
+print(formatter.format(True,False,False,True))
+print(formatter.format(formatter,formatter,formatter,formatter))
+print(formatter.format(
+       'In hearts entwined, a gentle flame ignites\n',
+       'A dance of souls, through days and nights\n',
+       'With every beat , loves melody plays\n',
+       'A timeless bond that forever stays'))
