a14f7eae387440de3842dd00676affc6c18f60a4
jishnu <givemeakiss111@gmail.com>
2023-10-28 21:48:56 +0530

    new

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name= 'Jishnu'
+my_age = 26
+my_height=168  #cm
+my_weight = 58  #kg
+my_eyes = 'Black'
+my_teeth =  'White'
+my_hair ='Black'
+
+print (f"Let's talk about {my_name}.")
+print(f"He's {my_height} cm tall.")
+print(f"He's {my_weight} kg heavy.")
+print(f"Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total =my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get  {total}.")
